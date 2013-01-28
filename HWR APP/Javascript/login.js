da1112722218b922357e09780e574b4b6aceceee
Tobi09 <t.lentz@gmx.net>
2013-01-28 11:38:27 +0100

    try LOGIN 27.01

--- HWR APP/Javascript/login.js
+++ HWR APP/Javascript/login.js
@@ -14,33 +14,45 @@ function checkPreAuth() {
 }
 
 function handleLogin() {
+	console.log("handleLogin");
     var form = $("#loginForm");    
     //disable the button so we can't resubmit while we wait
 	$("#submitButton",form).attr("disabled","disabled");
     var u = $("#username", form).val();
     var p = $("#password", form).val();
-    console.log("click");
     if(u != '' && p!= '') {
-        $.post("http://garten-kabel-pflasterbau.de/hwr-com/hallodu.php", {un:u,pw:p}, function(res) {
-			alert(res);
-            if(res.logincheck == 'true') {
-                //store
+		console.log("serverabfrage");
+		console.log("username: " + u);
+		console.log("password: " + p);
+		var url = "http://garten-kabel-pflasterbau.de/hwr-com/hallodu.php?un="+u+"&pw="+p;
+		console.log(url);  
+		$.post(url, function (data) {
+			console.log("request gesendet");
+			console.log(data.logincheck);
+			if (data == '({"logincheck":"true"});') {
+				//store
                 window.localStorage["username"] = u;
-                window.localStorage["password"] = p;             
-                $.mobile.changePage("Kontakte.html");
-            } else {
-                navigator.notification.alert("Your login failed", function() {});
-            }
-         $("#submitButton").removeAttr("disabled");
-        },"json");
-    } else {
+                window.localStorage["password"] = p;    
+				console.log("gehe zur naechsten Seite");
+				$.mobile.changePage("Kontakte.html");
+				//navigator.notification.alert("Your login succesed", function() {});
+			} else {
+				console.log("keine Daten");
+				console.log("false login")
+				alert("Your login failed");
+				navigator.notification.alert("Your login failed", function() {});
+			}
+		});
+    } else { 
         //Thanks Igor!
-        notification.alert("You must enter a username and password", function() {});
-        $("#submitButton").removeAttr("disabled");
+		console.log("keine angaben");
+		alert("You must enter a username and password");
+		//navigator.notification.alert("You must enter a username and password");
     }
+	$("#submitButton").removeAttr("disabled");
     return false;
 }
 
-function deviceReady() {
+function deviceReady() {  
  $("#loginForm").on("submit",handleLogin);
 }
